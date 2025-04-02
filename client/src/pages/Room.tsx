8ca1fe0d7d05346f2e6fd70cf4104d42066fea8b
herseyy <herseyodasco1@gmail.com>
2025-04-02 23:50:59 +0800

    add text editor

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -1,9 +1,55 @@
 import { useRef, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import axios from "axios"
 import "../static/room.css";
+
+import { EditorState } from "@codemirror/state";
+import { EditorView, keymap } from "@codemirror/view";
+import {indentWithTab} from "@codemirror/commands";
+import { basicSetup } from "codemirror";
+import { python } from "@codemirror/lang-python";
+
 
 function Room() {
 	const location = useLocation();
+	const editorRef = useRef<HTMLDivElement | null>(null);
+	const inputRef = useRef<HTMLTextareaElement | null>(null);
+	const viewRef = useRef<EditorView | null>(null);
+
+	useEffect(() => {
+		if (!editorRef.current) return;
+
+		const state = EditorState.create({
+			doc: "",
+			extensions: [
+				basicSetup,
+				keymap.of([indentWithTab]),
+				python()
+			]
+		});
+
+		const view = new EditorView({
+			state,
+			parent: editorRef.current
+		})
+
+		viewRef.current = view;
+    	return () => view.destroy();
+	}, [])
+
+	const runCode = async() => {
+		if (!viewRef.current) return;
+		let code = viewRef.current.state.doc.text
+		let input = inputRef.current.value.split("\n")
+		
+		const res = axios.post("http://localhost:3000/run", {
+			language: "python",
+			code,
+			input
+		}).catch(err => {
+			console.log(`ERROR posting code ${err}`)
+		})
+	}
 
 	return (
 		<div className="container">
@@ -15,14 +61,38 @@ function Room() {
 			<div className="main-container">
 				<div className="members-container">
 					<h1>ROOM: {location.state.roomId}</h1>
-					<p>sideBar members</p>
+					<div className="sidebar-members">
+						<div className="member">
+							<p>username1 | points</p>
+							<button>attack</button>
+						</div>
+					</div>
 				</div>
 				<div className="editor-container">
-					main code editor
-					<div style={{ height: "400px", border: "1px solid #ccc" }}></div>
+					<p>main code editor</p>
+					<div ref={editorRef} className="code-editor" id="code-editor"/>
+					<div className="input-output">
+						<div>
+							<p>Input:</p>
+							<textarea ref={inputRef} className="input-editor" />
+						</div>
+						<div>
+							<p>Output:</p>
+							<div className="output-area"> </div>
+						</div>
+					</div>
+					<button className="run-btn" onClick={runCode}>Run</button>
 				</div>
 				<div className="status-container">
-					status
+					<select>
+						<option>Trial</option>
+					</select>
+					<button>Submit</button>
+					<p>Submissions</p>
+					<div className="submission-area">
+						<p>problem | status</p>
+						<p>points</p>
+					</div>
 				</div>
 			</div>
 		</div>
--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 
 import roomRoutes from "./routes/room.route.js"
 
+import piston from "piston-client"
+
 const app = express();
 const PORT = 3000;
 
@@ -12,11 +14,27 @@ app.use(cors({
 	]
 }))
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/rooms", roomRoutes);
 
 app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
+
+app.post("/run", async (req, res) => {
+	console.log(req.body)
+	console.log("hhehekfoafi")
+
+	const client = piston({ server: "https://emkc.org" });
+	const runtimes = await client.runtimes();
+	const result = await client.execute(
+		"python",
+		"print('Hellllo World')"
+	)
+	console.log(result)
+})
 
 app.listen(PORT, () => {
 	console.log(`server listening to ${PORT}`)
